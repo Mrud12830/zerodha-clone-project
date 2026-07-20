52cd1d93624cf9341054fee9fa4fab452ce909a7
Mrudula Bidwai <mrudulabidwai30@gmail.com>
2026-07-20 18:59:42 +0530

    Initial commit

---
+++ dashboard/src/components/Dashboard.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Apps from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
+import axios from "axios";
+import { useEffect } from "react";
+
+const Dashboard = () => {
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+        const token = localStorage.getItem("token");
+
+        if(!token){
+            window.location="/login";
+            return;
+        }
+
+        try{
+
+            const res = await axios.get(
+                "http://localhost:3002/dashboard",
+                {
+                    headers:{
+                        Authorization:`Bearer ${token}`
+                    }
+                }
+            );
+
+            console.log(res.data);
+
+        }
+
+        catch(err){
+
+            alert("Unauthorized");
+
+            localStorage.removeItem("token");
+
+            window.location="/login";
+
+        }
+
+    };
+
+    fetchData();
+
+},[]);
+  return (
+    <div className="dashboard-container">
+      <GeneralContextProvider>
+        <WatchList />
+      </GeneralContextProvider>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+      <button
+    onClick={() => {
+        localStorage.removeItem("token");
+        window.location = "/login";
+    }}
+    style={{
+        position: "absolute",
+        top: "20px",
+        right: "20px",
+        padding: "10px 20px",
+        cursor: "pointer"
+    }}
+>
+    Logout
+</button>
+    </div>
+  );
+};
+
+export default Dashboard;
