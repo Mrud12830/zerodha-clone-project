83e24f2d15535d7ee781bc18dce840cf6ab3d9b0
Mrudula Bidwai <mrudulabidwai30@gmail.com>
2026-07-21 21:24:27 +0530

    fix login navigate issue

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 function Login() {
-  const navigate = useNavigate();
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
