48aeda6e29511b407cd104fa237b5921b6e9a18e
Mrudula Bidwai <mrudulabidwai30@gmail.com>
2026-07-21 22:18:38 +0530

    Fix pricing hero image issue

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -9,7 +9,7 @@ function Hero() {
       </div>
       <div className="row p-5 mt-5 text-center">
         <div className="col-4 p-4">
-          <img src="images/pricingEquity.svg" alt="pricing hero.js image" />
+          <img src="images/pricingEquity.svg" alt="pricing hero.js " />
           <h1 className="fs-3">Free equity delivery</h1>
           <p className="text-muted">
             All equity delivery investments (NSE, BSE), are absolutely free — ₹
@@ -17,7 +17,7 @@ function Hero() {
           </p>
         </div>
         <div className="col-4 p-4">
-          <img src="images/intradayTrades.svg" alt="pricing hero2.js image" />
+          <img src="images/intradayTrades.svg" alt="pricing hero2.js " />
           <h1 className="fs-3">Intraday and F&O trades</h1>
           <p className="text-muted">
             Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
@@ -26,7 +26,7 @@ function Hero() {
           </p>
         </div>
         <div className="col-4 p-4">
-          <img src="images/pricingEquity.svg" alt="pricing hero3.js image" />
+          <img src="images/pricingEquity.svg" alt="pricing hero3.js" />
           <h1 className="fs-3">Free direct MF</h1>
           <p className="text-muted">
             All direct mutual fund investments are absolutely free — ₹ 0
