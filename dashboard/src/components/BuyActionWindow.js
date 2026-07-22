76f021841ec364bf2bd701d6e4f97a5ff1556a95
Mrudula Bidwai <mrudulabidwai30@gmail.com>
2026-07-23 00:32:35 +0530

    Fix buy action URL

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("http://zerodha-clone-project-gd4n.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
